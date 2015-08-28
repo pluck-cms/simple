99cbcde40539884c6d96fdec85ea01982ca2fc64
Bill Creswell <billcreswell@gmail.com>
2015-08-28 19:47:16 -0400

    The Data File
    This file contains the meta information for the module

---
+++ simple.php
@@ -0,0 +1,12 @@
+<?php
+//{plugin}_info
+  function simple_info() {
+    $module_info = array(
+        'name'          => 'My Simple Demo Module',
+        'intro'         => 'The Simplest Module',
+        'version'       => '0.1',
+        'author'        => 'grwebguy',
+        'website'       => 'http://twitter.com/grwebguy',
+        'compatibility' => '4.7'
+    );
+ }
